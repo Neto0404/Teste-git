043a782e35c232641bb59932dc929582a3a9cfbd
Claudinier Neto <nosimprovisos@gmail.com>
2020-07-23 10:43:32 -0300

    Ideia pronta com erro de repeticao de pergunta do valor inicial do caixa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace Italia_Lancheria_ControleCaixa
     {
         static void Main(string[] args)
         {
-            
+            while (DateTime.Now.Hour < 14 && DateTime.Now.Hour > 6)
+            {
+                Console.WriteLine("Qual o valor inicial do caixa?");
+                double ValorInicialCaixa = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("Caixa Aberto Com : " + ValorInicialCaixa);
+
+                Console.WriteLine("incira o valor da nota que o cliente pagou: ");
+                double notaC = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Quanto o Cliente Gastou? : ");
+                double contaCliente = double.Parse(Console.ReadLine());
+
+                double troco = (notaC - contaCliente);
+
+                if (notaC < contaCliente)
+                {
+                    Console.WriteLine("Ainda falta dinheiro");
+                }
+                else
+                {
+
+                    Console.WriteLine("Valor do troco: " + troco);
+                }
+                double ValorAtualCaixa = ((ValorInicialCaixa + notaC) - troco);
+                Console.WriteLine("Valor atual no caixa é de: "+ValorAtualCaixa);
+
+
+            }
         }
     }
 }
