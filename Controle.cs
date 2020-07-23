2ba7c00c1e58e9b884734b3c6ce21ca2cfc076e3
Claudinier Neto <nosimprovisos@gmail.com>
2020-07-23 12:19:19 -0300

    Sistema Basico pronto

---
+++ Controle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Italia_Lancheria_ControleCaixa
+{
+    class Controle
+    {
+        public static double ValorInicialCaixa { get; set; }
+        public static double ValorAtualCaixa  { get; set;}
+        public static float i { get; set; }
+        
+        public static double Controlar()
+        {
+            Console.WriteLine();
+
+
+            Console.WriteLine("incira o valor da nota que o cliente pagou: ");
+            double notaC = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quanto o Cliente Gastou? : ");
+            double contaCliente = double.Parse(Console.ReadLine());
+
+            double troco = (notaC - contaCliente);
+
+            if (notaC < contaCliente)
+            {
+                Console.WriteLine("Ainda falta dinheiro");
+            }
+            else
+            {
+
+                Console.WriteLine("Valor do troco: " + troco);
+            }
+            
+            
+            if (i == 0)
+            {
+                ValorAtualCaixa = ((ValorInicialCaixa + notaC) - troco);
+                i++;
+                Console.WriteLine("Valor atual no caixa é de: " + ValorAtualCaixa);
+                return ValorAtualCaixa;
+
+            }
+            else
+            {
+                ValorAtualCaixa = ((ValorAtualCaixa + notaC) - troco);
+                i++;
+                Console.WriteLine("Valor atual no caixa é de: " + ValorAtualCaixa);
+                return ValorAtualCaixa;
+            }
+        }
+        
+        public static double Caixa(double valor)
+        {
+            
+            ValorInicialCaixa =valor;
+            return ValorInicialCaixa;
+        }
+    }
+}
